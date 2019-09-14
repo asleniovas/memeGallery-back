dde177443fc9bf018518d87264cd0f4ab5b258b8
asleniovas <asleniovas@gmail.com>
2019-09-14 16:31:14 +0300

    cors

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 var vehicleRoutes = require('./routes/memeRoutes.js');
 
 //key modules
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
-const favicon = require('serve-favicon')
+const favicon = require('serve-favicon');
+const cors = require('cors');
 
 var app = express()
 
-//express initialisation
+//express initialisation and required middlewares
 app
 
   .use(express.static(path.join(__dirname, 'public')))
@@ -18,10 +19,11 @@ app
   .use('/controllers', express.static(process.cwd() + '/controllers'))
   .use(bodyParser.urlencoded({extended: false}))
   .use(bodyParser.json())
+  .use(cors())
   .use(favicon(path.join(__dirname, 'favicon.ico')))
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
 //route initialisation
-vehicleRoutes(app)
+vehicleRoutes(app);
