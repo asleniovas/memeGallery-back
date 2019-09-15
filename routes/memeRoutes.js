ed189cbb7d838187900f39d75c1bad594cddca7d
asleniovas <asleniovas@gmail.com>
2019-09-15 22:56:03 +0300

    comments

--- routes/memeRoutes.js
+++ routes/memeRoutes.js
@@ -4,14 +4,16 @@ module.exports = function (app) {
    
     var memeHandler = new MemeHandler();
     
-    //GET all memes
+    //GET all memes and POST a new meme
     app.route('/api/memes')
         .get(memeHandler.getAllMemes)
         .post(memeHandler.insertMeme)
 
+    //DELETE a meme
     app.route('/api/memes/:memeId')
         .delete(memeHandler.deleteMeme)
 
+    //GET index page
     app.route('/')
         .get(memeHandler.getIndexView)
 
